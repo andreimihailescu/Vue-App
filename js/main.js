a2f2aaf0bd636b08160248557415666f3522a1f5
Andrei Mihailescu <andrei.mihailescu994@gmail.com>
2018-03-18 16:19:50 +0200

    First commit

---
+++ js/main.js
@@ -0,0 +1,140 @@
+Vue.component('task-list', {
+    template: `
+        <div>
+            <task v-for="task in tasks">{{ task.task }}</task>
+        </div>
+    `,
+    data() {
+        return {
+            tasks: [
+                { task: 'Go to the store', completed: true },
+                { task: 'Go to the movie', completed: false },
+                { task: 'Go to the bank', completed: true },
+            ]
+        }
+    }
+});
+
+Vue.component('task', {
+    template: '<li><slot></slot></li>'
+});
+
+Vue.component('message', {
+    props: ['title', 'body'],
+    data() {
+        return {
+            isVisible: true
+        };
+    },
+    template: `
+        <article class="message" v-show="isVisible">
+            <div class="message-header">
+                <p>{{ title }}</p>
+                <button @click="hideModal" class="delete" aria-label="delete"></button>
+            </div>
+            <div class="message-body">
+                {{ body }}
+            </div>
+        </article>
+    `,
+    methods: {
+        hideModal() {
+            this.isVisible = false;
+        }
+    }
+});
+
+Vue.component('modal', {
+    template: `
+        <div class="modal is-active">
+            <div class="modal-background"></div>
+                <div class="modal-content">
+                    <div class="box">
+                        <slot></slot>
+                    </div>
+                </div>
+            <button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+        </div>
+    `
+})
+
+Vue.component('tabs', {
+    template: `
+        <div>
+            <div class="tabs">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.isActive }">
+                        <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }}</a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="tabs-details">
+                <slot></slot>
+            </div>
+        </div>
+    `,
+    data() {
+        return {
+            tabs: []
+        };
+    },
+    created() {
+        this.tabs = this.$children;
+    },
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.isActive = (tab.name == selectedTab.name);
+            });
+        }
+    }
+});
+
+Vue.component('tab', {
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+    template: `
+        <div v-show="isActive"><slot></slot></div>
+    `,
+    data() {
+        return {
+            isActive: false
+        };
+    },
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+    mounted() {
+        this.isActive = this.selected
+    }
+});
+
+Vue.component('coupon', {
+    template: `
+        <input placeholder="Enter your coupon code" @blur="onCouponApplied">
+    `,
+    methods: {
+        onCouponApplied() {
+            this.$emit('applied');
+        }
+    }
+});
+
+new Vue({
+    el: '#root',
+    data: {
+        showModal: false,
+        modalMessage: 'Blablabla',
+        couponApplied: false
+    },
+    methods: {
+        onCouponApplied() {
+            this.couponApplied = true;
+        }
+    }
+});
